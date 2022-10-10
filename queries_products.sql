f10cf79662c015bbad6ea1f20b312b2a4ec710b3
Alec Dieken <alec@alecdieken.com>
2022-10-10 15:29:55 -0700

    added all exercise files

---
+++ queries_products.sql
@@ -0,0 +1,37 @@
+-- Comments in SQL Start with dash-dash --
+INSERT INTO products (name, price, can_be_returned)
+VALUES ('chair', 44.00, false);
+
+INSERT INTO products (name, price, can_be_returned)
+VALUES ('stool', 25.99, true);
+
+INSERT INTO products (name, price, can_be_returned)
+VALUES ('table', 124.00, false);
+
+SELECT * FROM products;
+
+SELECT name FROM products;
+
+SELECT name, price FROM products;
+
+INSERT INTO products (name, price, can_be_returned)
+VALUES ('gold-plated-triple-bunk-bed', 9999.99, false);
+
+SELECT * FROM products WHERE can_be_returned;
+
+SELECT * FROM products 
+WHERE price < 44;
+
+SELECT * FROM products 
+WHERE price BETWEEN 22.50 AND 99.99;
+
+UPDATE products
+SET price = CAST((price - 20) as decimal(16, 2));
+
+DELETE FROM products WHERE price < 25;
+
+UPDATE products
+SET price = CAST((price + 20) as decimal(16, 2));
+
+UPDATE products
+SET can_be_returned = true;
